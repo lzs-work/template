e224aa2d472d7a667dfc2c6fa3036926ff077e19
GitHub Action <action@github.com>
2021-09-09 08:47:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/lzswork/template/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/lzswork/template/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.lzswork.template.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lzswork.template.MyBundle
 
 class MyApplicationService {
 
